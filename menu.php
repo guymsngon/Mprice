c1dcb9f9a30bd53b9d46c98f517b46a5be69a02e
guyms ngon <guymsfaithful@gmail.com>
2018-07-09 11:56:11 +0100

    mon menu

---
+++ menu.php
@@ -0,0 +1,85 @@
+        <nav class="navbar navbar-default navbar-fixed white bootsnav on no-full no-background">
+
+            <div class="container">            
+                <!-- Start Atribute Navigation -->
+                <div class="attr-nav">
+                    <!--<ul>
+                        <li class="side-menu"><a href="#">-->
+                        <form method="post" action="#">
+                        	<p>
+                        		<label for="Cathégories"><h4>Quels Cathégorie de produit voulez-vous Consulter? </h4></label><br/>
+                        		<select name="Cathégories" id="Cathégories">
+                        			<option value="Agro alimentaire"> Agro alimentaire</option>
+                        			<option value="Numérique">Numérique</option>
+                        			<option value="Matériaux de Construction">Matériaux de Construction</option>
+                        		</select>
+                        	</p>
+                        	<button type="submit" class="btn submit_btn">Allons y</button>
+                        </form>
+                        <!--<p> </p> <i class="fa fa-bars"></i></a></li>
+                    </ul>-->
+                </div>
+                <!-- End Atribute Navigation -->
+
+                <!-- Start Header Navigation -->
+                <div class="navbar-header">
+                    <button data-target="#navbar-menu" data-toggle="collapse" class="navbar-toggle" type="button">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <a href="#brand" class="navbar-brand">
+                        <!--<img alt="" class="logo logo-display" src="images/logo.png">
+                        <img alt="" class="logo logo-scrolled" src="images/logo.png">-->
+                    </a>
+                </div>
+                <!-- End Header Navigation -->
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse text-uppercase" id="navbar-menu" >
+                    <ul data-out="fadeOutUp" data-in="fadeInDown" class="nav navbar-nav">
+                        <li role="presentation"><a href="#home">Accueil</a></li>
+                        <li role="presentation"><a href="#space member">Se Connecter</a></li>
+                        <li role="presentation"><a href="#About">Abonnement</a></li>
+                        <li role="presentation"><a href="#research">Recherche</a></li>
+                        <li role="presentation"><a href="#help">Aide</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div>   
+        </nav>
+
+        <header id="home">
+            <!-- Carousel -->
+            <div id="carousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel" data-slide-to="1"></li>
+                    <li data-target="#carousel" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="images/m2.jpg" alt="market-price"/>
+
+                        <div class="container caption_area">
+                            <div class="carousel-caption">
+                                <h3>LE PRIX DES PRODUITS SUR LE MARCHER</h3>
+                                <h1>MARKET PRICE</h1>
+                                <p>Obtenez les prix de vos produits favorie en temps réels...</p>
+
+                                <a href="" class="banner_btn">achat en ligne </a>
+                            </div>					
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/m3.jpg" alt="market-price"/>
+                        <a href="" class="banner_btn">achat en ligne </a>
+                    </div>
+                    <div class="item">
+						<img src="images/m1.jpg" alt="market-price"/>
+						<a href="" class="banner_btn">achat en ligne </a>
+                    </div>
+                </div>
+            </div><!-- Carousel end-->
+
+        </header><!-- Header end -->
