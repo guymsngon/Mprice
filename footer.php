b98bba5d2d14b5e230f63c118b6b4922fb02fc36
guyms ngon <guymsfaithful@gmail.com>
2018-07-09 12:25:52 +0100

    mon projet market price

---
+++ footer.php
@@ -0,0 +1,29 @@
+        <footer>
+            <div class="container text-canter">
+                <ul class="list-inline footer_menu">
+                    <li role="presentation"><a href="#">Accueil</a></li>
+                    <li role="presentation"><a href="#">Crée un compte</a></li>
+                    <li role="presentation"><a href="#">Abonnement</a></li>
+                    <li role="presentation"><a href="#">shop</a></li>
+                    <li role="presentation"><a href="#">Services</a></li>
+                    <li role="presentation"><a href="#">Contact</a></li>
+                </ul>
+                <a class="footer_logo" href="#"><img src="images/logo.png" alt="bgstudio" /></a>
+
+                <ul class="list-inline social">
+                    <li role="presentation"><a href="#"><span class="fa fa-facebook"></span></a></li>
+                    <li role="presentation"><a href="#"><span class="fa fa-twitter"></span></a></li>
+                    <li role="presentation"><a href="#"><span class="fa fa-linkedin"></span></a></li>
+                </ul>
+
+                <div class="footer-copyright text-center">
+                    <p class="wow fadeInRight" data-wow-duration="1s">
+                        Made with 
+                        <i class="fa fa-heart"></i>
+                        by 
+                        <a target="_blank" href="http://bootstrapthemes.co">Market-Price</a>
+                        2017. All Rights Reserved
+                    </p>
+                </div>
+            </div>
+        </footer>
